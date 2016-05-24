b48f337b658f4b1be3e75aaea75e1a03599fe559
Shimpei Kodama <shimpei-kodama@cookpad.com>
2016-05-24 12:58:16 +0900

    [Refactor] Reduce locks and transactions while loading data

--- lib/bricolage/streamingload/loader.rb
+++ lib/bricolage/streamingload/loader.rb
@@ -31,46 +31,35 @@ module Bricolage
       def execute
         @params.ds.open {|conn|
           @connection = conn
+          # Do nothing if task already running or succeeded
+          # Retry if error
+          return if task_processed?(@params.task_seq)
           assign_task
+          #FIXME unprocessed job remains if loader dies this here, before finishing load
+          #You can retry it by inserting result as error if the task message is still in sqs
           do_load
         }
       end
 
       def assign_task
-        success = true
         @connection.transaction {|txn|
-          @connection.lock('dwh_tasks')
-          @connection.lock('dwh_jobs')
-          @connection.lock('dwh_job_results')
-
-          already_processed = task_processed?(@params.task_seq)
-          table_in_use = table_in_use?(@params.schema, @params.table)
-
-          write_job(@params.task_seq)
-          @job_seq = read_job_seq(@job_id)
+          @job_seq = write_job(@params.task_seq)
           @logger.info "dwh_job_seq=#{@job_seq}"
-
-          # These result record MUST be written in the same transaction.
-          # DO NOT raise exception here
-          if already_processed
-            # Skip already processed task (may happen with second and later "duplicated" sqs messages)
-            #FIXME May leave object unprocessed when running multiple loader concurrently
-            write_job_result 'success', 'task is already processed by other jobs'
-          elsif table_in_use
-            write_job_error 'failure', "other jobs are working on the target table"
-            success = false
-          end
+          stage_files(@params.task_seq)
         }
-        raise JobFailure, "could not assign task: #{@params.task_id}" unless success
       end
 
       def do_load
-        staged_files = stage_files(@params.task_seq)
-        return if staged_files.empty?
+        files = staged_files
+        if files.empty?
+          @connection.transaction {
+            commit_job_result
+          }
+        end
         ManifestFile.create(
           @params.ctl_bucket,
           job_id: @job_id,
-          object_urls: staged_files,
+          object_urls: files,
           logger: @logger
         ) {|manifest|
           if @params.enable_work_table?
@@ -113,30 +102,29 @@ module Bricolage
                 )
             ;
         EndSQL
-      end
-
-      def read_job_seq(job_id)
-        @connection.query_value(<<-EndSQL)
+        job_seq = @connection.query_value(<<-EndSQL)
             select dwh_job_seq
             from dwh_jobs
-            where dwh_job_id = #{s job_id}
+            where dwh_job_id = #{s @job_id}
             ;
         EndSQL
+        job_seq
       end
 
       def task_processed?(task_seq)
         n_nonerror_jobs = @connection.query_value(<<-EndSQL)
             select
-                count(*)
+                count(distinct dwh_job_seq)
             from
-                dwh_tasks t
-                inner join dwh_jobs j using (dwh_task_seq)
-                left outer join dwh_job_results r using (dwh_job_seq)
+                dwh_jobs t
+            left outer join
+                dwh_job_results
+            using(dwh_job_seq)
             where
                 t.dwh_task_seq = #{task_seq}
                 and (
-                    r.status is null         -- running
-                    or r.status = 'success'  -- finished with success
+                    status is null -- runnnig
+                    or status = 'success'
                 )
             ;
         EndSQL
@@ -144,26 +132,9 @@ module Bricolage
         n_nonerror_jobs.to_i > 0
       end
 
-      def table_in_use?(schema, table)
-        n_working_jobs = @connection.query_value(<<-EndSQL)
-            select
-                count(*)
-            from
-                dwh_tasks t
-                inner join dwh_jobs j using (dwh_task_seq)
-                left outer join dwh_job_results r using (dwh_job_seq)
-            where
-                t.schema_name = #{s schema}
-                and t.table_name = #{s table}
-                and r.status is null   -- running
-            ;
-        EndSQL
-        @logger.debug "n_working_jobs=#{n_working_jobs.inspect}"
-        n_working_jobs.to_i > 0
-      end
-
       def stage_files(task_seq)
         # already-processed object_url MUST be removed
+        # retry errored objects
         @connection.execute(<<-EndSQL)
             insert into dwh_str_load_files
                 ( dwh_job_seq
@@ -172,22 +143,29 @@ module Bricolage
             select
                 #{@job_seq}
                 , object_url
-            from
-                dwh_str_load_files_incoming
-            where
-                dwh_task_seq = #{task_seq}
-                and object_url not in (
-                    select
-                        f.object_url
-                    from
-                        dwh_str_load_files f
-                        inner join dwh_job_results r using (dwh_job_seq)
-                    where
-                        r.status = 'success'
+            from (
+                select
+                    object_url
+                from
+                    dwh_str_load_files_incoming
+                where
+                    dwh_task_seq = #{task_seq}
+                except
+                select
+                    object_url
+                from
+                    dwh_str_load_files
+                left outer join
+                    dwh_job_results
+                using(dwh_job_seq)
+                where status is null -- runnnig
+                or status = 'success' -- success
                 )
             ;
         EndSQL
+      end
 
+      def staged_files
         staged_files = @connection.query_values(<<-EndSQL)
             select
                 object_url
@@ -198,7 +176,6 @@ module Bricolage
             ;
         EndSQL
         @logger.debug "n_staged_files=#{staged_files.size}"
-
         staged_files
       end
 
