5991451829552591afad3827c6cb7c452827f17a
Minero Aoki <aamine@loveruby.net>
2017-02-10 22:28:57 +0900

    load: Optimize the number of transactions

--- jobclass/load.rb
+++ jobclass/load.rb
@@ -26,14 +26,47 @@ JobClass.define('load') {
 
   script {|params, script|
     script.task(params['dest-ds']) {|task|
-      task.drop_force_if params['drop']
-      task.exec params['table-def'] if params['table-def']
-      task.truncate_if params['truncate']
-      task.load params['src-ds'], params['src-file'], params['dest-table'],
-          params['format'], params['jsonpath'], params['options']
-      task.vacuum_if params['vacuum'], params['vacuum-sort']
-      task.analyze_if params['analyze']
-      task.grant_if params['grant'], params['dest-table']
+      if params['drop']
+        # drop - create - load pattern
+        unless params['table-def']
+          raise ParameterError, "--table-def is required when --drop is enabled"
+        end
+
+        task.transaction {
+          task.drop_force '${dest_table}'
+          task.exec params['table-def']
+          task.load params['src-ds'], params['src-file'], params['dest-table'],
+              params['format'], params['jsonpath'], params['options']
+          task.grant_if params['grant'], params['dest-table']
+        }
+        # ANALYZE, VACUUM is needless for newly loaded table, skip always.
+
+      elsif params['truncate']
+        # truncate - load pattern
+        if params['drop'] or params['table-def']
+          raise ParameterError, "--truncate and --drop/--table-def is exclusive"
+        end
+
+        task.truncate_if params['truncate']
+        task.transaction {
+          task.load params['src-ds'], params['src-file'], params['dest-table'],
+              params['format'], params['jsonpath'], params['options']
+          task.grant_if params['grant'], params['dest-table']
+        }
+        # ANALYZE, VACUUM is needless for newly loaded table, skip always.
+
+      else
+        # load only pattern
+
+        task.transaction {
+          task.load params['src-ds'], params['src-file'], params['dest-table'],
+              params['format'], params['jsonpath'], params['options']
+          task.grant_if params['grant'], params['dest-table']
+          task.analyze_if params['analyze']
+        }
+        # We cannot execute VACUUM in transaction
+        task.vacuum_if params['vacuum'], params['vacuum-sort']
+      end
     }
   }
 }
