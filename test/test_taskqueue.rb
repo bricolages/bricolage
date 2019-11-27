1122a6e2ced5280ab97e055de02356149b9655db
Ichinari Sato <ichinari-sato@cookpad.com>
2019-11-27 20:11:30 +0900

    Modify databasetaskqueue test for new lock strategy

--- test/test_taskqueue.rb
+++ test/test_taskqueue.rb
@@ -52,48 +52,61 @@ module Bricolage
     context = Context.for_application(home_path='test/home')
     datasource = context.get_data_source('psql', 'test_db')
     jobnet_path = Pathname.new('test/home/subsys/net1.jobnet')
-    jobnet = RootJobNet.load_auto(context, [jobnet_path]).jobnets.first
-    jobrefs = jobnet.refs - [jobnet.start, *jobnet.net_refs, jobnet.end]
+    jobnet_ref = RootJobNet.load_auto(context, [jobnet_path]).jobnets.first
+    jobrefs = jobnet_ref.refs - [jobnet_ref.start, *jobnet_ref.net_refs, jobnet_ref.end]
+
     jobtask1 = JobTask.new(jobrefs.pop)
     jobtask2 = JobTask.new(jobrefs.pop)
 
+    job_dao = Bricolage::DAO::Job.new(datasource)
+    jobnet_dao = Bricolage::DAO::JobNet.new(datasource)
+    jobnet = jobnet_dao.find_or_create('subsys','net1')
+    job1 = job_dao.find_or_create('subsys', 'job1', jobnet.id)
+    job2 = job_dao.find_or_create('subsys', 'job2', jobnet.id)
+
     teardown do
-      queue = DatabaseTaskQueue.restore_if_exist(datasource, jobnet)
-      queue.unlock
+      queue = DatabaseTaskQueue.restore_if_exist(datasource, jobnet_ref, 'dummy_executor')
       queue.clear
     end
 
+    test "#enqueue/#dequeuing/#dequeued" do
+      queue = DatabaseTaskQueue.restore_if_exist(datasource, jobnet_ref, 'dummy_executor')
+      assert_equal 0, queue.size
+      queue.enqueue jobtask1
+      assert_equal 1, queue.size
+      queue.dequeuing
+      assert_equal 1, queue.size
+      queue.dequeued
+      assert_equal 0, queue.size
+    end
+
     test "DatabaseTaskQueue.restore_if_exist" do
-      queue1 = DatabaseTaskQueue.restore_if_exist(datasource, jobnet)
+      queue1 = DatabaseTaskQueue.restore_if_exist(datasource, jobnet_ref, 'dummy_executor')
       assert_equal 0, queue1.size
       queue1.enqueue jobtask1
       queue1.enqueue jobtask2
       queue1.dequeuing
-      queue2 = DatabaseTaskQueue.restore_if_exist(datasource, jobnet)
+      queue2 = DatabaseTaskQueue.restore_if_exist(datasource, jobnet_ref, 'dummy_executor')
       assert_equal 2, queue2.size
     end
 
-    test "#save" do
-      queue = DatabaseTaskQueue.restore_if_exist(datasource, jobnet)
-      assert_false queue.queued?
-      queue.enqueue jobtask1
-      assert_true  queue.queued?
-    end
-
-    test "#lock" do
-      queue = DatabaseTaskQueue.restore_if_exist(datasource, jobnet)
-      queue.enqueue jobtask1
+    test "#lock_jobnet/#unlock_jobnet" do
+      queue = DatabaseTaskQueue.restore_if_exist(datasource, jobnet_ref, 'dummy_executor')
+      assert_false  queue.locked?
+      queue.lock_jobnet
+      assert_true  queue.locked?
+      queue.unlock_jobnet
       assert_false queue.locked?
-      queue.lock
-      assert_true  queue.locked?
     end
 
-    test "#unlock" do
-      queue = DatabaseTaskQueue.restore_if_exist(datasource, jobnet)
-      queue.enqueue jobtask1
-      queue.lock
+    test "#lock_job/#unlock_job" do
+      queue = DatabaseTaskQueue.restore_if_exist(datasource, jobnet_ref, 'dummy_executor')
+      assert_false  queue.locked?
+
+      queue.lock_job(job1.id)
       assert_true  queue.locked?
-      queue.unlock
+
+      queue.unlock_job(job1.id)
       assert_false queue.locked?
     end
   end
