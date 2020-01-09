9dfaabac3acf891360e0192f4bcf1bbd76a0f254
Ichinari Sato <ichinari-sato@cookpad.com>
2020-01-09 22:24:57 +0900

    Fix queue order of restored job execution

--- lib/bricolage/taskqueue.rb
+++ lib/bricolage/taskqueue.rb
@@ -233,7 +233,7 @@ module Bricolage
                                                status: [Bricolage::DAO::JobExecution::STATUS_WAIT,
                                                         Bricolage::DAO::JobExecution::STATUS_RUN,
                                                         Bricolage::DAO::JobExecution::STATUS_FAILURE])
-      job_executions.each { |je| enqueue(je) }
+      job_executions.sort_by(&:job_execution_id).each { |je| enqueue(je) }
     end
 
     def enqueue_job_executions
